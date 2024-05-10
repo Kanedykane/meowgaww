59dd24b1cab51fbc531bd579649a325c984d5016
Ma. Christina Kane Vito <91617687+Kanedykane@users.noreply.github.com>
2024-05-10 10:48:30 +0800

    blogs

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,21 +1,8 @@
-// Select the heart button and add event listener
-const likeBtn = document.querySelector('.like-btn');
+// Get the like button element
+var likeBtn = document.getElementById('likeBtn');
+
+// Add a click event listener to the like button
 likeBtn.addEventListener('click', function() {
-    // Toggle the 'liked' class to change the heart color
-    this.classList.toggle('liked');
-    
-    // Get the count of likes
-    let count = parseInt(this.textContent);
-    
-    // If the post is liked, increment the count, else decrement
-    if (this.classList.contains('liked')) {
-        count++;
-        // Show alert message when post is added to favorites
-        alert('Post added to favorites!');
-    } else {
-        count--;
-    }
-    
-    // Update the like count
-    this.textContent = count;
+  // Toggle the 'liked' class to change the color
+  likeBtn.classList.toggle('liked');
 });
