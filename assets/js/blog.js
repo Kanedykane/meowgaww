b84f4aaf20f957a245d0de1464d2186c7e199751
Ma. Christina Kane Vito <91617687+Kanedykane@users.noreply.github.com>
2024-05-10 10:55:18 +0800

    blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,8 +1,23 @@
-// Get the like button element
-var likeBtn = document.getElementById('likeBtn');
-
-// Add a click event listener to the like button
-likeBtn.addEventListener('click', function() {
-  // Toggle the 'liked' class to change the color
-  likeBtn.classList.toggle('liked');
-});
+$('.btn-counter').on('click', function(event, count) {
+    event.preventDefault();
+    
+    var $this = $(this),
+        count = $this.attr('data-count'),
+        active = $this.hasClass('active'),
+        multiple = $this.hasClass('multiple-count');
+    
+    // First method, allows to add custom function
+    // Use when you want to do an ajax request
+    /* if (multiple) {
+    $this.attr('data-count', ++count);
+    // Your code here
+    } else {
+    $this.attr('data-count', active ? --count : ++count).toggleClass('active');
+    // Your code here
+    } */
+    
+    // Second method, use when ... I dunno when but it looks cool and that's why it is here
+    $.fn.noop = $.noop;
+    $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
+    
+  });
